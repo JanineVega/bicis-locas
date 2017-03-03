0eaf282b032bbab6130ab7e90a669f58ab2cda6b
Janine Vega <osmary.vega@gmail.com>
2017-03-03 16:48:12 -0500

    casi terminado

--- js/main.js
+++ js/main.js
@@ -1,53 +1,45 @@
 function validateForm(){
 	/* Escribe tú código aquí */
 
-
-  var validarButton = document.getElementById("validar");
-
-    var inputName = document.getElementById("name");
-    var nombreError = document.getElementById("nombre-error");
-
-    if (inputName.value == "") {
-      nombreError.innerHTML = "Ingrese un nombre válido";
-    } else {
-      nombreError.innerHTML = "";
-    }
-
-    var lastname = document.getElementById("lastname");
+// ----------------Validando Nombre-------------------------------------------------
+    var inputName    = document.getElementById("name").value;
+    var nombreError  = document.getElementById("nombre-error");
+    var lastname      = document.getElementById("lastname").value;
     var lastnameError = document.getElementById("lastname-error");
-
-    if (lastname.value == "") {
-      lastnameError.innerHTML = "Ingrese un apellido válido";
-    } else {
-      lastnameError.innerHTML = "";
-    }
-
-    var correo= document.getElementById("input-email");
+    var correo     = document.getElementById("input-email").value;
     var emailError = document.getElementById("email-error");
+    var contraseña      = document.getElementById("input-password").value;
+    var contraseñaError = document.getElementById("password-error");
+    var indice      = document.getElementById("opciones").selectedIndex;
+    var indiceError = document.getElementById("indice-error");
+    var boton=document.getElementById("validar");
 
-    if (correo.value == "") {
-      emailError.innerHTML = "Ingrese un email válido";
-    } else {
-      emailError.innerHTML = "";
-    }
 
-    var contra= document.getElementById("input-password");
-    var passwordError = document.getElementById("password-Error");
 
-    if (contra.value == null ||
-       == 0 || /^\s+$/.test(valor) ) {
-      passwordError.innerHTML = "Ingrese un password válido";
-    } else {
-      passwordError.innerHTML = ;
-    }
-    if (contra.value == null ||
-       == 0 || /^\s+$/.test(valor) ) {
-      passwordError.innerHTML = "Ingrese un password válido";
-    } else {
-      passwordError.innerHTML = ;
-    }
+      if (inputName.length == 0 || inputName == null || !(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/.test(inputName))){
+        nombreError.innerHTML="La primera letra con mayúscula"
+        alert("Ingrese un nombre válido la primera letra con mayúscula");
 
+        return false;
+      }else if (!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/.test(lastname)) || lastname == null ) {
+      alert("Ingrese un apellido válido, la primera letra en mayúscula");
+      return true;
+      } else if (!(/^[\w]+@{1}[\w]+\.+[a-z]{3}$/.test(correo))){
+      alert("La dirección de email " + correo + " no es correcta.");
+      return true;
+      } else if (!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,15}/.test(contraseña))|| contraseña=="password" || contraseña=="123456" || contraseña == "098754"){
+      alert("La contraseña es invalida! ");
+      contraseñaError.innerHTML = "Debe ser mayor o igual a 6 digitos contener letras y números, almenos un carácter y una letra mayúscula.";
+      return true;
+    }else if (indice == null || indice==0) {
+          alert("Seleccione una opción");
+          return false;
+        }
 }
+
+
+
+
 /*
 var validarButton=document.getElementsByClassName("btn")[0];
 validarButton.addEventListener('click',function() {
